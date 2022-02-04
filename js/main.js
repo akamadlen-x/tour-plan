69065317cf80f7b01f009cf98999c4af13fd7ab6
akamadlen-x <akamadlen@gmail.com>
2022-02-04 17:55:57 +0300

    fix: footer bottom

--- js/main.js
+++ js/main.js
@@ -1,42 +1,53 @@
-const swiperAuto = new Swiper(".hotel-slider", {
-  loop: true,
-  autoplay: {
-    delay: 4000,
-    reverseDirection: true,
-    disableOnInteraction: false,
-    pauseOnMouseEnter: true,
-  },
-});
-const swiperMouse = new Swiper(".hotel-slider", {
-  // Optional parameters
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: ".hotel-slider__button--next",
-    prevEl: ".hotel-slider__button--prev",
-  },
-});
-const swiperKeyboard = new Swiper(".hotel-slider", {
-  loop: true,
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-});
-const swiperReviews = new Swiper(".reviews-slider", {
-  // Optional parameters
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: ".reviews-slider__button--next",
-    prevEl: ".reviews-slider__button--prev",
-  },
-});
+$(document).ready(function () {
+  const swiperAuto = new Swiper(".hotel-slider", {
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      reverseDirection: true,
+      disableOnInteraction: false,
+      pauseOnMouseEnter: true,
+    },
+  });
+  const swiperMouse = new Swiper(".hotel-slider", {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: ".hotel-slider__button--next",
+      prevEl: ".hotel-slider__button--prev",
+    },
+  });
+  const swiperKeyboard = new Swiper(".hotel-slider", {
+    loop: true,
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
+  const swiperReviews = new Swiper(".reviews-slider", {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: ".reviews-slider__button--next",
+      prevEl: ".reviews-slider__button--prev",
+    },
+  });
 
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener("click", function () {
-  console.log("Клик по кнопке");
-  document
-    .querySelector(".navbar-bottom")
-    .classList.toggle("navbar-bottom--visible");
+  var menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener("click", function () {
+    console.log("Клик по кнопке");
+    document
+      .querySelector(".navbar-bottom")
+      .classList.toggle("navbar-bottom--visible");
+  });
+
+  var modalButton = $("[data-toggle=modal]");
+  modalButton.on("click", openModal);
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
+  }
 });
